83c47f23cbfd5ef50dc3b9f446bc63353d315b09
Johan Castillo <jcjohan2707@gmail.com>
2021-06-07 16:02:55 -0400

    Init Template

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import ContentWrapper from "../components/ContentWrapper"
+import Sidebar from "../components/Sidebar"
+
+
+
+const Home = () => {
+    return(
+        <>
+            <Sidebar />
+            <ContentWrapper />
+            
+        </>
+    )
+}
+
+
+
+export default Home
+
+
